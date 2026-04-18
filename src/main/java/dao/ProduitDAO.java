8e1394096012cf5f7d1216addf22b0bdb1091b3e
EssAya <essouiri.aya@etu.uae.ac.ma>
2026-04-18 18:42:47 +0100

    Update ProduitDAO.java

--- src/main/java/dao/ProduitDAO.java
+++ src/main/java/dao/ProduitDAO.java
@@ -6,7 +6,7 @@ import model.Produit;
 public interface ProduitDAO {
     void addProduit(Produit p);
     void deleteProduit(Long id);
-    Produit getProduitById(Long id);
+    Produit getProduitById(Long id);  
     List<Produit> getAllProduits();
     void updateProduit(Produit p);
 }
